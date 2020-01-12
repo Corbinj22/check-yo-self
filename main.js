0af37014445ee528534247387de6ca8964395763
Justin Corbin <jcorbin2290@gmail.com>
2020-01-11 18:40:42 -0700

    add task to staging area js complete

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ var userTaskItem = document.querySelector('.task-item-input');
 var searchBar = document.querySelector('.search-input');
 var addTaskButton = document.querySelector('.task-button');
 var searchButton = document.querySelector('.search-button');
-var createTaskButton = document.querySelector('.form-button')
+var createTaskList = document.querySelector('.form-button')
 var clearButton = document.querySelector('.clear-button');
 var filterButton = document.querySelector('.filter-button');
 var leftColumn = document.querySelector('.left-column');
-
+var taskStagingArea = document.querySelector('.updated-task-box');
+var createTaskButton = document.querySelector('.task-button');
 
 // Event listeners
 leftColumn.addEventListener('keyup', enableAddTaskButton);
 clearButton.addEventListener('click', clearInputField);
+// createTaskList.addEventListener('click', addTaskToStagingArea)
+createTaskButton.addEventListener('click', addTaskToStagingArea)
 
 
 //toggle state of all buttons left-section
@@ -23,6 +26,12 @@ function enableAddTaskButton() {
 
 //Clear user input fields
 function clearInputField() {
-userTaskItem.value = '';
-userTaskName.value = '';
+  userTaskItem.value = '';
+  userTaskName.value = '';
+  enableAddTaskButton();
+}
+
+//Add task item to staging area
+function addTaskToStagingArea() {
+  taskStagingArea.insertAdjacentHTML("beforeend",`<p><button class='task-button-clear'>X</button>${userTaskItem.value}</p>`);
 }
